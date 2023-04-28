b9ab17536805c6fd3da94f4f294ccbcc12b506ec
Mark Scott-Kiddie <mwscottkiddie@gmail.com>
2023-04-28 18:27:32 +0100

    FIX: image centering

--- src/.vuepress/theme/components/PostListItem.vue
+++ src/.vuepress/theme/components/PostListItem.vue
@@ -42,11 +42,14 @@ a {
 
 .img {
   overflow: hidden;
-  align-content: center;
+  align-items: center;
+  justify-content: center;
+  display: flex;
 }
 
 img {
-  height: 100%;
-  object-fit: cover;
+  flex-shrink: 0;
+  min-width: 100%;
+  min-height: 100%;
 }
 </style>
